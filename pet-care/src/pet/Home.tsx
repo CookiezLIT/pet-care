dcf477c0757e68b81ed7a049060a9b38911c4e4d
Dan-Lucian Nita <dan-luciannita@Dan-Lucians-MacBook-Air.local>
2022-10-28 14:10:59 +0300

    feat: Refactored folder structure

---
+++ pet-care/src/pet/Home.tsx
@@ -0,0 +1,23 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Home.css';
+
+const Home : React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Pet Details</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Blank</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Home;
