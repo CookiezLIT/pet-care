97a9cd6d958fe2e14b149ca7b0fb0aa1d23b3d26
Dan-Lucian Nita <dan-luciannita@Dan-Lucians-MacBook-Air.local>
2022-11-05 15:32:03 +0200

    feat: Added refresh after post

--- pet-care/src/hooks/usePetList.tsx
+++ pet-care/src/hooks/usePetList.tsx
@@ -5,7 +5,8 @@ import {IPetList} from '../interfaces/IpetList'
 const usePetList =  () => {
     const [petList, setPetList] = useState<IPetList[]>([])
     const handleAddPet = () => {
-        axios.post("http://127.0.0.1:8000/pets/add_pet/", {name : "Spic", date_of_birth : "2022-06-15", is_adopted : "True", age : 1})
+        axios.post("http://127.0.0.1:8000/pets/add_pet/", {name : "Spic", date_of_birth : "2022-06-15", is_adopted : "True", age : 1});
+        window.location.reload();
     }
 
     const fetchPets = useCallback(() => {
