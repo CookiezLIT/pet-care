799e8e923aa735d791dd5524ff09296fb0e9f338
Dan-Lucian Nita <dan-luciannita@Dan-Lucians-MacBook-Air.local>
2022-11-14 22:37:34 +0200

    feat:api: Added jwt autenthication

--- api/api/api/settings.py
+++ api/api/api/settings.py
@@ -59,6 +59,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
